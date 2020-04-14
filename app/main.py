99c1ab20b3ea7a0d5e7cfb69841870980ac80f9b
Chuck Musser <cmusser@noip.com>
2020-04-13 18:11:56 -0700

    Add a human readable timestamp to the output.
    This is derived from the generated _id value, which embeds a
    POSIX timestamp indicating the insert time.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from bson import ObjectId
+from datetime import datetime
 from flask import request, Flask
 import json
 from pymongo import MongoClient
+
 
 # The unique ID field is a type that can't be serialized, so this
 # custom JSON encoder turns it into a string and handles everything
@@ -23,11 +25,21 @@ def get_db():
     client = MongoClient('mongodb://db:27017/')
     return client.roomme
 
+
+def add_timestamp(location):
+    if '_id' in location:
+        ts = datetime.fromtimestamp(int.from_bytes(bytes.fromhex(
+            str(location['_id']))[0:4], byteorder='big')).isoformat()
+        location.update({'ts': ts})
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def get_all():
     return 'iot-roomme!'
+
 
 @app.route('/locations', methods=['POST'])
 def create_new():
@@ -35,20 +47,26 @@ def create_new():
         new_location = request.get_json()
         id = get_db().locations.insert_one(new_location).inserted_id
         return 'request accepted (id {})'.format(id)
+
     except TypeError as e:
         print(request)
         return 'bad request: {}'.format(e), 400
 
+
 @app.route('/locations')
 def list_locations():
     all_locations = list(get_db().locations.find())
+    for location in all_locations:
+        add_timestamp(location)
+
     return JSONEncoder().encode(all_locations)
-        
+
+
 @app.route('/locations/id/<id>')
 def get_location(id):
     location = get_db().locations.find_one({"_id": ObjectId(id)})
     if location is None:
         return 'id {} not found'.format(id), 404
     else:
+        add_timestamp(location)
         return JSONEncoder().encode(location)
-
